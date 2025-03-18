eb5eb28f0921c07ac87d9b113f114e987f2b7ee9
MattSanchez77 <matthew.sanchez77@icloud.com>
2025-03-17 23:22:37 -0600

    More Changes

---
+++ script.js
@@ -0,0 +1,30 @@
+// for (let i = 0; i < 100; i++) {
+// 	console.log(i);
+// }
+// x = 100
+// for (let x = 1; x <= 100; x++) {
+//     if (x % 3 === 0 && x % 5 == 0)
+//     console.log(`${x} FizzBuzz`);} 
+//         { if (x % 3 == 0) {
+//             console.log(`${x} Fizz`); } 
+//         if (x % 5 == 0) {
+//             console.log(`${x} Buzz`);} else {
+//                 console.log(x);
+                
+//             }
+//     }
+
+for (n = 1; n < 100; n++){
+    if(n % 3 == 0 && n % 5 == 0){
+        console.log(`${n} Fizz`);
+    } else {
+        if(n % 3 ==0){
+            console.log(`${n} Buzz`);{
+                
+            } else {
+                if (n % 5 == 0)
+            }
+            
+        }
+    }
+}
